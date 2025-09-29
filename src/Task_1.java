cf123bf6110a859bee60c85ebda243d1ef8031b1
616732 <miller.616732@swl.k12.oh.us>
2025-09-29 08:45:49 -0400

    9/29/25 8:45

---
+++ src/Task_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Task_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double tempC;
+        double tempF;
+        boolean valid = false;
+        do {
+            System.out.println("Please enter a temperature to be converted to the better version:");
+            if (scan.hasNextDouble()){
+                tempC = scan.nextDouble();
+                tempF = (tempC*9/5) + 32;
+                System.out.printf("Temperature (Celsius): %10.2f", tempC);
+                System.out.printf("\nTemperature (Fahrenheit): %10.2f", tempF);
+                valid = true;
+            }else{
+                System.out.println("You must enter a valid temperature");
+                scan.nextLine();
+            }
+        } while (!valid);
+
+    }
+}
