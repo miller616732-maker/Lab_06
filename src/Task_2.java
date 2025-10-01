82a62e35fabc33e0380a8386df554f6d9b370e9e
616732 <miller.616732@swl.k12.oh.us>
2025-10-01 08:55:23 -0400

    8:55 10/01/2025

--- src/Task_2.java
+++ src/Task_2.java
@@ -2,33 +2,63 @@ import java.util.Scanner;
 public class Task_2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double yourGallons;
-        double yourMPG;
-        double yourGalPrice;
+        double yourGallons = 0;
+        double yourMPG = 0;
+        double yourGalPrice = 0;
         double yourTotal;
         double distanceLeft;
-        boolean valid = false;
+        boolean validGal = false;
+        boolean validMPG = false;
+        boolean validPrice = false;
         do {
             System.out.println("Enter your current gallons of gasoline");
-            if (scan.hasNextDouble()){
-                if () {
-                }
+            if (scan.hasNextDouble()) {
                 yourGallons = scan.nextDouble();
-            } else {
-                System.out.println("Please enter a valid number");
-            }
+                if (yourGallons < 0) {
+                    scan.nextLine();
+                    System.out.println("Please enter a valid number");
+                } else {
+                    validGal = true;
+                }
+            }else{
+                scan.nextLine();
+                System.out.println("Enter a valid number");
+        }
+        } while (!validGal) ;
+        do {
             System.out.println("Enter your miles per gallon");
             if (scan.hasNextDouble()) {
                 yourMPG = scan.nextDouble();
                 if (yourMPG < 0) {
+                    scan.nextLine();
                     System.out.println("Please enter a valid number");
+                } else {
+                    validMPG = true;
                 }
             } else {
+                scan.nextLine();
                 System.out.println("Enter a valid number");
             }
+        } while (!validMPG);
+        do {
+            System.out.println("Enter your cost per gallon");
             if (scan.hasNextDouble()) {
                 yourGalPrice = scan.nextDouble();
+                if (yourGalPrice < 0) {
+                    scan.nextLine();
+                    System.out.println("Please enter a valid number");
+                } else {
+                    validPrice = true;
+                }
+            } else {
+                scan.nextLine();
+                System.out.println("Enter a valid number");
             }
-        } while (valid != false);
+        } while (!validPrice);
+        yourTotal = 100/yourMPG * yourGalPrice;
+        distanceLeft = yourMPG * yourGallons;
+        System.out.printf("%-10s%10.2f","Your price per gallon is:",yourTotal);
+        System.out.printf("\n%-10s%10.2f","Your remaining miles are",distanceLeft);
+
     }
 }
